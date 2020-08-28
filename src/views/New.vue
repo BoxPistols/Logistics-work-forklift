c63fe0a5704fcb49026cdbf93309865308c4e442
ait <ait@ex.in>
2020-08-28 15:33:37 +0900

    [page] create New.vue / ui to textarea

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,17 +1,88 @@
 <template>
   <div>
     <h2>New</h2>
-      <div class="editor">
-          <textarea name="memo" cols="30" rows="10"></textarea>
-          <button>保存</button>
+    <div class="editor__lay">
+      <div class="editor__org">
+        <div class="editor__mol textarea">
+          <textarea class="textarea-edit" name="memo" :value="body"></textarea>
+        </div>
+        <div class="editor__mol button">
+          <button class="button-save" @click="save">保存</button>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data: () => {
+    return {
+      body: "Hello"
+    };
+  },
+  methods: {
+    save() {
+      // alert();
+      // this.$router.push('/')
+    }
+  }
+};
+</script>
+
 <style lang="stylus" scoped>
-textarea
-  width 100%
-  max-width 800px
-button
-  display block
+/*
+  variable setting
+*/
+
+//colors
+gray-base = #8c97a1
+active = teal
+
+// mixin
+bd(color)
+  border: 2px solid color
+// extend
+active-form
+  bd(active)
+  outline none
+  background-color lighten(forestgreen, 95%)
+
+
+/*
+  view design
+*/
+.editor
+  &__lay
+    max-width 800px
+    margin auto
+  &__org
+    display flex
+    flex-direction column
+  &__mol
+    display flex
+    &.textarea
+      .textarea-edit
+        bd(gray-base)
+        width 100%
+        min-height 300px
+        margin-bottom 12px
+        padding 1em
+        font-size 1.5em
+        &:active, &:focus
+          @extend active-form
+
+    &.button
+      justify-content flex-end
+      .button-save
+        bd(gray-base)
+        line-height 1em
+        padding 1em 4em
+        background-color white
+        transition: .3s
+        &:hover
+          cursor pointer
+          background-color #eee
+        &:active, &:focus
+          @extend active-form
 </style>
